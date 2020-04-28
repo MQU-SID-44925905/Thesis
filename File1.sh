fc082c9f730912ddee41bf524d623a75384c3b12
MQU-SID-44925905 <64466917+MQU-SID-44925905@users.noreply.github.com>
2020-04-28 19:51:56 +1000

    Add files via upload

---
+++ File1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# File 1
+#This File is designed to download, run and test cFS installed on Linux to Ensure Basic Compatibility
+#
+#v0.1 - Follows instructions stated on https://github.com/nasa/cFS
+#Errors:
+#
+
+
+#Setup
+# To setup the cFS BUNDLE directly from the latest set of interoperable repositories
+git clone https://github.com/nasa/cFS.git
+cd cFS
+git submodule init
+git submodule update
+
+#Copy in the default makefile and definitions:
+cp cfe/cmake/Makefile.sample Makefile
+cp -r cfe/cmake/sample_defs sample_defs
+
+#Build and Run
+#To prep, compile, and run on the host
+
+make SIMULATION=native prep
+make
+make install
+cd build/exe/cpu1/
+./core-cpu1
